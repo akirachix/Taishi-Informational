89d5bd34d2f46ae50272a25b031e3540a0f4bf0f
Lavender <anyangolavenderr@gmail.com>
2024-09-13 20:53:21 +0300

    added feature About

--- themis-informational/tailwind.config.ts
+++ themis-informational/tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,9 +7,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+      'nh': { 'raw': '(min-width: 1020px) and (max-width: 1024px), (min-width: 1280px) and (max-width: 1280px)' },
+
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        josefin: ['"Josefin Sans"', 'sans-serif'],
       },
     },
   },
